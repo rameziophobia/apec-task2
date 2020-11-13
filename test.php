55d7a691e7701c67caf1fe2b8b9302350635df1e
Marwan Yasser <marwanyasser133@gmail.com>
2020-11-14 00:33:27 +0200

    more questions , 10 rand questions
    more questions for debugging and displaying random questions using PHP

---
+++ test.php
@@ -0,0 +1,56 @@
+<?php
+//include "top.php";
+include "question.php";
+function question_showcase($question, $answers, $number) {
+    shuffle($answers);
+    echo '<div class="container mt-4">';
+    echo '<div class="form-group ">';
+    echo '<div class="row">';
+    echo '<h4 style="color: white;">'.$number.' '.$question.'</h4>';
+    echo '</div>';
+    echo '<div class="row d-flex justify-content-around mt-3">';
+    for($i = 0; $i < 4; $i++) {
+        echo '<div class="form-check">';
+        echo '<input class="form-check-input" type="radio" name="exampleRadios';
+        echo ($i+1);
+        echo '" id="exampleRadios2"';
+        echo 'value="';
+        echo $answers[i] , '">';
+        echo '<label class="form-check-label" for="exampleRadios';
+        echo ($i+1) , '">';
+        echo $answers[$i],'</label>';
+        echo '</div>';
+    }
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+}
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "apec2020";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM ExamQuestions";
+$result = mysqli_query($conn, $sql);
+$questionList = array();
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        array_push($questionList , new question($row['question'], array($row['choice1'], $row['choice2'], $row['choice3'], $row['choice4'])));
+    }
+}
+
+shuffle($questionList);
+for($x = 0; $x < 10; $x++) {
+    question_showcase($questionList[$x]->question, $questionList[$x]->choices, $x+1);
+}
+//include "bottom.php";
+?>
